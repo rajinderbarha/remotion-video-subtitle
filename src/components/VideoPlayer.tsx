b502464009438f1d0b501013a0c99525128d8e1b
rajinderbarha <rajinderbarha@gmail.com>
2024-10-07 17:53:00 +0530

    responsive

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -2,15 +2,17 @@ import { Composition, Video, useCurrentFrame, interpolate, interpolateColors } f
 import transcript from "../assets/transcript.json";
 
 const VideoPlayer: React.FC = () => {
-    
-    const frame = useCurrentFrame();
 
+    const frame = useCurrentFrame(); // render app on every frame
+
+    // To group the captions that fall in a single second time frame in an array
     const groupCaptionsbySecond = (transcript: any) => {
-        const groupedCaptions: any[][] = [];
+        const groupedCaptions: any[][] = []; // created a array of arrays for every word that falls in single time frame
         transcript.forEach((caption: any) => {
-            if (caption.type === "punctuation") return;
+            if (caption.type === "punctuation") return;  // To skip the punctuations 
             const startTime = Math.floor(caption.start_time);
 
+            // if the captions is not present in array then push it in grouped captions (groupedCaptions)
             if (!groupedCaptions[startTime]) {
                 groupedCaptions[startTime] = [];
             }
@@ -21,37 +23,44 @@ const VideoPlayer: React.FC = () => {
 
     const groupedCaptionsArray = groupCaptionsbySecond(transcript);
     // console.log(groupedCaptionsArray)
-    const colors = ["yellow", "green", "red"];
+    const colorScheme = ["yellow", "green", "red"]; //array of color scheme to change color of subtitles
 
     return (
         <div className="w-full flex justify-center items-center">
-            <Video src={require("../assets/input.mp4")} />
+            {/* video component to display input video*/}
+            <Video src={require("../assets/input.mp4")} />  
 
 
+            {/* Subtitle component */}
             {groupedCaptionsArray.map((captionsInSecond, second) => {
 
-                const startFrame = second * 21;
-                const endFrame = (second + 1) * 21;
+                const startFrame = second * 21;  {/* to calculate frame per second. we are using 21 frames per second */ }
+                const endFrame = (second + 1) * 21;  {/* we are using one second time frame to display subtitles(i.e. second + 1 for one second time frame)*/ }
+
+                {/* opacity of subtitles for fade in and fade out */}
                 const opacity = interpolate(
                     frame,
                     [startFrame, startFrame + 10, endFrame - 10, endFrame],
                     [0.5, 1, 1, 0.5],
                     { extrapolateRight: "clamp", extrapolateLeft: "clamp" }
-                );
-                
-                const color = colors[second % colors.length];
+                );  
 
+                {/* color of subtitles  (changes every second based on each second) */}
+                const color = colorScheme[second % colorScheme.length];
+
+
+                {/*  if condition so that subtitles caption should be displayed during that particular section of the video. */ }
                 if (frame >= startFrame && frame < endFrame) {
                     return (
                         <div
                             key={second}
-                            className="absolute bottom-60 w-full text-center text-2xl font-bold  text-white"
-                            style={{opacity, color}}
+                            className="absolute bottom-[158px] w-full text-center text-2xl font-bold  text-white"
+                            style={{ opacity, color }} 
                         >
-
-                            <div >
+                            <div className="text-[28px] w-max mx-auto font-extrabold bg-[#0000005c]">
                                 {captionsInSecond.map((caption: any) => (
                                     <span key={caption.id}>
+                                        {/* check to conditionally display break and show subtitle in two lines  */}
                                         {caption.break ? (
                                             <>
                                                 {caption.text} <br />
@@ -65,6 +74,7 @@ const VideoPlayer: React.FC = () => {
 
 
                             <div className="mt-2">
+                                {/*  if condition to display emoji */ }
                                 {captionsInSecond.map((caption: any) => (
                                     caption.emoji ? (
                                         <span key={caption.id} className="text-xl">
